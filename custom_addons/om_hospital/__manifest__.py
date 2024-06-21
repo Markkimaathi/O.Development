6bbcc7f9b3c0413bab976a1f69087e37f621069a
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-21 16:52:28 +0300

    manifest

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Management',
+    'Version': '1.0.0',
+    'sequence': -100,
+    'author': ' tegrds  ',
+    'website': 'https:// ',
+    'category': 'Health',
+    'summary': 'Hospital management system',
+    'description': 'Hospital management system',
+    'depends': ['mail'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto-install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
