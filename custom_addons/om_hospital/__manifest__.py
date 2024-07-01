6a362394de38b258846ef08a38674ea5acd15365
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-01 13:18:18 +0300

    access rights

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -8,6 +8,7 @@
     'description': """Hospital management system""",
     'depends': [],
     'data': [
+        'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
     ],
