9c78e377fc6767dad10a9e8158b7803c0f855f54
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-03 10:13:48 +0300

    archive, unarchive

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -9,3 +9,4 @@ class HospitalPatient(models.Model):
     ref = fields.Char(string='Reference')
     age = fields.Integer(string='Age')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    active = fields.Boolean(string="Active", default=True)
