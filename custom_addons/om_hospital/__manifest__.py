ac48db5ca02cc08a7b84f9597164aa753c8395ce
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-03 15:52:34 +0300

    chatter

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Hospital',
     'summary': 'Hospital management system',
     'description': """Hospital management system""",
-    'depends': [],
+    'depends': ['mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -3,6 +3,7 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Name')
