5eb9633b36603381bad017ffef1724de9588fe0e
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-03 09:08:03 +0300

    search view defined

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -6,5 +6,6 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Name')
+    ref = fields.Char(string='Reference')
     age = fields.Integer(string='Age')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
