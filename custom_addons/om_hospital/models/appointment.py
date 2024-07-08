06b2232fd75779cfb634b1d5297965b22a35d409
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 15:47:42 +0300

    related

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -6,8 +6,9 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient")
+    gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
-    booking_date = fields.Date(string='Booking date', default=fields.Date.context_today)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
 
 
 
