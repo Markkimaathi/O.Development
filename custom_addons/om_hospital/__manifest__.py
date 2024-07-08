9058422a905f364f3e38fc4b0574c875d64b7346
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 11:47:43 +0300

    appointment view

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -12,6 +12,7 @@
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
+        'views/appointment_view.xml',
     ],
     'demo': [],
     'auto-install': False,
