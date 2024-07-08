83d7fcc35b15adebaac20ed81c624042eba0e5c1
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 11:33:48 +0300

    appointment model

---
+++ custom_addons/om_hospital/models/appointment.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+class HospitalAppointment(models.Model):
+    _name = 'hospital.appointment'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Hospital Appointment'
+
