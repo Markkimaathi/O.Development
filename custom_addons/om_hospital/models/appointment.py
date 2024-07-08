1c97e89d4292a9f92ecb4242d9a7096b6779d6b8
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 12:45:43 +0300

    date/datetime

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -6,4 +6,8 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one('hospital.patient', string="Patient")
+    appointment_time = fields.Datetime(string='Appointment Time')
+    booking_date = fields.Date(string='Booking date')
+
+
 
