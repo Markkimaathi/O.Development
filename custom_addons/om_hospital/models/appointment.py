b688490370f2e2bfd5d2b7b9751d388e8190e499
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 16:29:06 +0300

    onchange function

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -9,6 +9,11 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference')
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
 
 
 
