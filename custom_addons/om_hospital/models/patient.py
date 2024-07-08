3fce1ad48b86086f6b40fee73acc5b3d5a9bc944
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-08 16:24:36 +0300

    computed field

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,3 +1,4 @@
+from datetime import date
 from odoo import api, fields, models
 
 
@@ -7,7 +8,18 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
     ref = fields.Char(string='Reference', default='Mark')
-    age = fields.Integer(string='Age', tracking=True)
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True, default='female')
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True,
+                              default='female')
     active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
