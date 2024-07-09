711f8b62f68d9996701186074bfede4a88c8533f
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-09 15:01:44 +0300

    priority

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -12,6 +12,11 @@ class HospitalAppointment(models.Model):
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
     ref = fields.Char(string='Reference')
     prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
