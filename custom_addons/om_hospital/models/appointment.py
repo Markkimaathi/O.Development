c0446762cfc27fccedd69c5a7d181e319212748d
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-10 10:52:15 +0300

    Many2one avatar/user

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -23,6 +23,7 @@ class HospitalAppointment(models.Model):
         ('in_consultation', 'In Consultation'),
         ('done', 'Done'),
         ('cancel', 'Cancelled')], default='draft', string="Status", required=True)
+    doctor_id = fields.Many2one('res.users', string='Doctor')
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
