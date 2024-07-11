e87a5dd2504a06e2b11004e23d2f87b01fcb7237
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-11 14:26:25 +0300

    Patient Tag

---
+++ custom_addons/om_hospital/models/patient_tag.py
@@ -0,0 +1,8 @@
+from odoo import api, fields, models
+
+class PatientTag(models.Model):
+    _name = "patient.tag"
+    _description = "Patient Tag"
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(string="Active", default=True)
