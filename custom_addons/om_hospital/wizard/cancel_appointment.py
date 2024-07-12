79e9d2abbd226ef92d9ae141fd6c9de9aa5d0e26
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-12 11:26:49 +0300

    Transient Model

---
+++ custom_addons/om_hospital/wizard/cancel_appointment.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = "cancel.appointment.wizard"
+    _description = "Cancel Appointment Wizard"
+
+    appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
