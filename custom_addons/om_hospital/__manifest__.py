beb7f1f27f4d1dc840d1c95235cdf9453b8bffe3
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-07-12 11:53:27 +0300

    Wizard-Cancel

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -9,6 +9,7 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
--- custom_addons/om_hospital/wizard/cancel_appointment.py
+++ custom_addons/om_hospital/wizard/cancel_appointment.py
@@ -5,3 +5,6 @@ class CancelAppointmentWizard(models.TransientModel):
     _description = "Cancel Appointment Wizard"
 
     appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
+
+    def action_cancel(self):
+        return
