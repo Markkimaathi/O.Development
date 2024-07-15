c3b34d3f2dd0b7ad8a37eba6985eaaff71b5f36b
Mark kimaaathi <markangelkimathi@gmail.com>
2024-07-15 18:54:20 +0300

    Merged Files

--- odoo-17.0/addons/purchase/models/__init__.py
+++ odoo-17.0/addons/purchase/models/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from . import account_invoice
 from . import account_tax
 from . import analytic_account
@@ -11,3 +14,5 @@ from . import product
 from . import res_company
 from . import res_config_settings
 from . import res_partner
+from . import purchase_rfq
+
