3ac4042048245d5b01e5f19de30c108d29b7f4b5
jmumo <jmumo6672@gmail.com>
2024-07-31 11:51:19 +0300

    status indicators, publish and active

--- custom_addons/tender_management/models/tender.py
+++ custom_addons/tender_management/models/tender.py
@@ -23,10 +23,15 @@ class TenderManagement(models.Model):
     days_to_deadline= fields.Integer(string='Days To Deadline', compute='_compute_days')
     bid_ids = fields.One2many('tender.bid', 'tender_id', string="Bids")
     bid_count = fields.Integer(string='Bid Count', compute='_compute_bid_count')
-    tender_management_line_ids=fields.One2many('tender.management.line','tender_management_id',string='Tender Management Line')
+    tender_management_line_ids=fields.One2many('tender.management.line','tender_management_id',string='Tender '
+                                                                                                      'Management Line')
     formatted_date = fields.Char(string='Formatted Date', compute='_compute_formatted_date')
     category=fields.Char(string='Category')
-    rank=fields.Char(string='Top Rank')
+    top_rank=fields.Char(string='Top Rank')
+    is_active = fields.Boolean(string='Active', default=True)
+    website_published = fields.Boolean('Publish on Website', copy=False)
+    # bid_count = fields.Integer(string='Bids', compute='_compute_bid_count')
+    rank = fields.Integer(string='Rank')
     @api.depends('date_created')
     def _compute_formatted_date(self):
         for record in self:
@@ -72,6 +77,11 @@ class TenderManagement(models.Model):
         if vals.get('ref', 'New') == 'New':
             vals['ref'] = self.env['ir.sequence'].next_by_code('tender.management') or 'New'
         return super(TenderManagement, self).create(vals)
+
+    def toggle_website_publish(self):
+        for record in self:
+            record.website_published = not record.website_published
+        return True
 
     class TenderManagementLine(models.Model):
         _name='tender.management.line'
